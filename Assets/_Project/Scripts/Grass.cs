2713e2e42fe6397b6e8df3fb3e6e7333844c834c
jeniferirwin <jenifer.irwin@gmail.com>
2021-12-03 20:10:29 -0500

    Grass cells can replenish properly

--- Assets/_Project/Scripts/Grass.cs
+++ Assets/_Project/Scripts/Grass.cs
@@ -2,7 +2,12 @@ using UnityEngine;
 
 namespace FoodChain
 {
-public class Grass : Organism
-{
-}
+    public class Grass : Organism
+    {
+        protected override void Die()
+        {
+            var cell = GridOperations.CellFromWorldPos(transform.position);
+            GridOperations.ClearCell(cell);
+        }
+    }
 }
